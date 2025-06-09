510a6ba07bc723b078b6e4540f5c640a77a00248
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2025-06-09 12:36:36 +0300

    deploy

---
+++ _app/immutable/chunks/Bx4D06dG.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1447x22)==null?void 0:s.base)??"/iqoption-islamic-account";var a;const e=((a=globalThis.__sveltekit_1447x22)==null?void 0:a.assets)??t;export{e as a,t as b};
---
+++ _app/immutable/chunks/DERui-zX.js
@@ -0,0 +1,1 @@
+import{w as a}from"./JsCKPF0r.js";const i=a("");export{i};
